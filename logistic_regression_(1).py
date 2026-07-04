890e2d8cf96bf47e9f1ee70098f20955006a7c08
richeekpandey07 <richeekpandey@gmail.com>
2026-07-04 14:43:00 +0530

    Comment out unused import for train_test_split
    Comment out the train_test_split import statement.

--- logistic_regression_(1).py
+++ logistic_regression_(1).py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import math
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
 
