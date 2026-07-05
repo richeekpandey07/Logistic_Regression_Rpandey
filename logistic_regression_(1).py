5a69052d7762aa7d669c722cd13aa97c2a5f710d
richeekpandey07 <richeekpandey@gmail.com>
2026-07-05 10:20:41 +0530

    Comment out prediction_function in logistic_regression
    Comment out the prediction_function and its logic.

--- logistic_regression_(1).py
+++ logistic_regression_(1).py
@@ -60,11 +60,11 @@ st.write(f"**Intercept (b):** {b:.4f}")
  # def sigmoid(x):
  #     return 1 / (1 + math.exp(-x))
 
-# def prediction_function(age):
-z = mx + b
-z = m * age + b 
-y = sigmoid(z)
-    # return y
+# # def prediction_function(age):
+# z = mx + b
+# z = m * age + b 
+# y = sigmoid(z)
+#     # return y
 
 import streamlit as st
 
